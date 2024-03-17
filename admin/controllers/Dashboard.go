73de43f78302c70d1105d9335475ac05e649761e
Melih Arık <55915116+meliharik@users.noreply.github.com>
2024-03-18 01:28:25 +0300

    List, Edit, Delete post UI and functionality

--- admin/controllers/Dashboard.go
+++ admin/controllers/Dashboard.go
@@ -2,10 +2,15 @@ package controllers
 
 import (
 	"fmt"
+	slug2 "github.com/gosimple/slug"
 	"github.com/julienschmidt/httprouter"
 	"goweb/admin/helpers"
+	"goweb/admin/models"
 	"html/template"
+	"io"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Dashboard struct{}
@@ -16,7 +21,10 @@ func (dashboard Dashboard) Index(w http.ResponseWriter, r *http.Request, params 
 		fmt.Println(err)
 		return
 	}
-	view.ExecuteTemplate(w, "index", nil)
+
+	data := make(map[string]interface{})
+	data["Posts"] = models.Post{}.GetAll()
+	view.ExecuteTemplate(w, "index", data)
 }
 
 func (dashboard Dashboard) NewItem(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
@@ -26,4 +34,59 @@ func (dashboard Dashboard) NewItem(w http.ResponseWriter, r *http.Request, param
 		return
 	}
 	view.ExecuteTemplate(w, "index", nil)
+}
+
+func (dashboard Dashboard) Add(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	title := r.FormValue("blog-title")
+	slug := slug2.Make(title)
+	description := r.FormValue("blog-desc")
+	categorID, _ := strconv.Atoi(r.FormValue("blog-category"))
+	content := r.FormValue("blog-content")
+
+	// upload
+	r.ParseMultipartForm(10 << 20)
+	file, header, err := r.FormFile("blog-picture")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	f, err := os.OpenFile("uploads/"+header.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = io.Copy(f, file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	models.Post{
+		Title:       title,
+		Slug:        slug,
+		Description: description,
+		Content:     content,
+		CategoryID:  categorID,
+		Picture_url: "uploads/" + header.Filename,
+	}.Add()
+
+	http.Redirect(w, r, "/admin", http.StatusSeeOther)
+	//TODO alert
+}
+
+func (dashboard Dashboard) Delete(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	post := models.Post{}.Get(params.ByName("id"))
+	post.Delete()
+	http.Redirect(w, r, "/admin", http.StatusSeeOther)
+}
+
+func (dashboard Dashboard) Edit(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	view, err := template.ParseFiles(helpers.Include("dashboard/edit")...)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	data := make(map[string]interface{})
+	data["Post"] = models.Post{}.Get(params.ByName("id"))
+	view.ExecuteTemplate(w, "index", data)
 }
--- config/Routes.go
+++ config/Routes.go
@@ -8,11 +8,16 @@ import (
 
 func Routes() *httprouter.Router {
 	router := httprouter.New()
+
 	// ADMIN
 	router.GET("/admin", admin.Dashboard{}.Index)
 	router.GET("/admin/yeni-ekle", admin.Dashboard{}.NewItem)
+	router.POST("/admin/add", admin.Dashboard{}.Add)
+	router.GET("/admin/delete/:id", admin.Dashboard{}.Delete)
+	router.GET("/admin/edit/:id", admin.Dashboard{}.Edit)
 
 	// Serve static files
 	router.ServeFiles("/admin/assets/*filepath", http.Dir("admin/assets"))
+	router.ServeFiles("/uploads/*filepath", http.Dir("uploads"))
 	return router
 }
