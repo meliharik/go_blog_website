7f9c181e05e1a16a5eb213311f56e0714ca5bc9e
Melih Arık <55915116+meliharik@users.noreply.github.com>
2024-03-17 23:49:42 +0300

    Post model mysql CRUD functions

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"fmt"
+	admin_models "goweb/admin/models"
 	"goweb/config"
 	"net/http"
 )
 
 func main() {
+	admin_models.Post{}.Migrate()
+	post := admin_models.Post{}.Get(1)
+	fmt.Println(post)
+
+	post.Delete(1)
+	fmt.Println(post)
 	http.ListenAndServe(":8080", config.Routes())
 }
